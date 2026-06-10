c6f9b1ca37b0a59a518b7d6a12ebbca89a0cc0fe
Ivan <ivan@example.com>
2026-06-10 22:26:04 +0300

    complete files fix

--- class/config.php
+++ class/config.php
@@ -1,16 +1,15 @@
 <?php
-define('SKIN_LANGUAGE', 'skins_en');
+define("BTM_LIBRARY", "skins_v2");
 
-define('DB_HOST', 'localhost');
-define('DB_PORT', '3306');
-define('DB_NAME', '');
-define('DB_USER', '');
-define('DB_PASS', '');
+define("DB_HOST", "95.213.255.10");
+define("DB_PORT", "3306");
+define("DB_NAME", "u4969_sdadsad");
+define("DB_USER", "u4969_vjwR1MsFuX");
+define("DB_PASS", "m4UGeLCh19dfPJs2_M27K");
 
-define('WEB_STYLE_DARK', true);
+define("WEB_STYLE_DARK", true);
 
-define('STEAM_API_KEY', 'CFD9C7353F93011A7FAC7CD6FBE973E4');
-define('STEAM_DOMAIN_NAME', 'adsadadadad');
-define('STEAM_LOGOUT_PAGE', '');
-define('STEAM_LOGIN_PAGE', '');
-
+define("STEAM_API_KEY", "CFD9C7353F93011A7FAC7CD6FBE973E4");
+define("STEAM_DOMAIN_NAME", "shiomi-cs2.onrender.com");
+define("STEAM_LOGOUT_POST", "");
+define("STEAM_LOGIN_PAGE", "https://shiomi-cs2.onrender.com/");
